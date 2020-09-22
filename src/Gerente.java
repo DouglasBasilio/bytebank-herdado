38cf91d4dce1ab15c5ab95a361227e3d23b600a5
DouglasBasilio <basilio.douglas@yahoo.com.br>
2020-09-22 12:46:27 -0300

    teste de herança

---
+++ src/Gerente.java
@@ -0,0 +1,24 @@
+// Gerente é um funcionário, Gerente herda da Classe Funcionario
+public class Gerente extends Funcionario {
+
+	private int senha;
+
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	public boolean autentica(int senha) {
+		if (this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	// Reescrita do método
+	public double getBonificacao() {
+		return super.getBonificacao() + super.getSalario(); 
+		// "salario" vem da classe super, então não uso this
+	}
+
+}
---
+++ src/TesteFuncionario.java
@@ -0,0 +1,15 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+
+		Funcionario douglas = new Funcionario();
+		douglas.setNome("Douglas Basilio");
+		douglas.setCpf("391.615.578-46");
+		douglas.setSalario(4000.00);
+		
+		System.out.println(douglas.getNome());
+		System.out.println(douglas.getBonificacao());
+	}
+
+}
